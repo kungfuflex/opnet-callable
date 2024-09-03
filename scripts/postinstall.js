993716db97761db5368313569324383b7a110f9b
kungfuflex <judoflexchop@gmail.com>
2024-09-02 20:12:16 -0500

    update

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,5 +2,6 @@
 
 import child_process from 'node:child_process';
 import path from 'node:path';
+console.log(import.meta.filename);
 process.chdir(path.join(path.parse(import.meta.filename).dir, '..', 'transform'));
 child_process.spawnSync('yarn', [], { stdio: 'inherit' });
